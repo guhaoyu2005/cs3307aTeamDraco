08f2d0ddf7c9532b51b57e572b632335ce827444
Haoyu Gu <hgu42@uwo.ca>
2016-10-20 15:00:40 -0400

    Added code comments for testcase 1.4

--- AcuitySTAR/src/testing/test_1.4.h
+++ AcuitySTAR/src/testing/test_1.4.h
@@ -7,13 +7,54 @@
 #include <QDebug>
 #include <QtTest/QtTest>
 
+
+/**
+ * @brief The QTTestNewSortOrder class
+ * @class QTTestNewSortOrder
+ * @author Haoyu Gu
+ * @date 2016/10/20
+ * @details
+ * QT Test Class for creating sort order function in mainwindow class
+ */
 class QTTestNewSortOrder : public QObject  {
     Q_OBJECT
 private slots:
+    /**
+     * @brief initTestCase
+     * @class QTTestNewSortOrder
+     * @author Haoyu Gu
+     * @date 2016/10/20
+     * @details
+     * Initialize the Test Case
+     */
     void initTestCase();
+    /**
+     * @brief cleanupTestCase
+     * @class QTTestNewSortOrder
+     * @author Haoyu Gu
+     * @date 2016/10/20
+     * @details
+     * Cleanup Tests
+     */
     void cleanupTestCase();
+    /**
+     * @brief endTestCase
+     * @class QTTestNewSortOrder
+     * @author Haoyu Gu
+     * @date 2016/10/20
+     * @details
+     * End Tests
+     */
     void endTestCase();
 
+    /**
+     * @brief createNewSortOrder
+     * @class QTTestNewSortOrder
+     * @author Haoyu Gu
+     * @date 2016/10/20
+     * @details
+     * Create new sort order, pass it to the UI componment and check.
+     */
     void createNewSortOrder();
 };
 
